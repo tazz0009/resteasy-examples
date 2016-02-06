c6a8f9ec5aa2c88cecba95366c94914539e69e86
Choi Seung Hwan <tazz0009@gmail.com>
2016-02-07 00:05:52 +0900

    initial commit

---
+++ src/main/java/com/tistory/tazz0009/Main.java
@@ -0,0 +1,12 @@
+package com.tistory.tazz0009;
+
+/**
+ * Enterprise Application Client main class.
+ *
+ */
+public class Main {
+    
+    public static void main( String[] args ) {
+        System.out.println( "Hello World Enterprise Application Client!" );
+    }
+}
